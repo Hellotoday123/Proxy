c755de438d185018b5231c2653986de09473add9
Viiridiz <amashkashareef@gmail.com>
2026-02-08 12:48:06 -0500

    firebase commit

---
+++ Proxy/Main/ProxyApp.swift
@@ -0,0 +1,28 @@
+//
+//  ProxyApp.swift
+//  Proxy
+//
+//  Created by user285973 on 2/8/26.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct ProxyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
